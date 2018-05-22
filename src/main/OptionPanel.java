286d67fb60d2a7c133a0d2f281070d2ce7dad6fd
kadlesim <kadlesim@fel.cvut.cz>
2018-05-22 17:33:09 +0200

    help button + change of selection method (from multiple to single selection) + some tooltips and label

--- src/main/OptionPanel.java
+++ src/main/OptionPanel.java
@@ -9,11 +9,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class OptionPanel extends JPanel {
-//    JButton showButton;
+    //    JButton showButton;
     JButton addButton;
     JButton removeButton;
     JButton clearButton;
-    JLabel infoLabel;
+    JButton helpButton;
+    JLabel infoLabel, infoLabel2;
     JList<Node> nodeJList, selectedNodesJList;
     Graph graph;
     JScrollPane listScroller;
@@ -43,29 +44,33 @@ public class OptionPanel extends JPanel {
 
 //            List<Node> toRemove = selectedNodesJList.getSelectedValuesList();
             Node nodeToRemove = selectedNodesJList.getSelectedValue();
-            int oldNodes = selectedNodesJList.getModel().getSize();
-            Node[] tmp = new Node[oldNodes - 1];
-            Node[] tmp2 = new Node[oldNodes];
-            int index = 0;
-            for (int i = 0; i < oldNodes; i++, index++) {
-                tmp2[i] = selectedNodesJList.getModel().getElementAt(i);
-            }
-
-            List<Node> result = new LinkedList<Node>();
-            for(Node item : tmp2) {
-                if (item.idx != nodeToRemove.idx) {
-                    result.add(item);
+            if (nodeToRemove != null) {
+                int oldNodes = selectedNodesJList.getModel().getSize();
+                Node[] tmp = new Node[oldNodes - 1];
+                Node[] tmp2 = new Node[oldNodes];
+                int index = 0;
+                for (int i = 0; i < oldNodes; i++, index++) {
+                    tmp2[i] = selectedNodesJList.getModel().getElementAt(i);
                 }
-            }
-            result.toArray(tmp);
-            selectedNodesJList.setListData(tmp);
 
-            for(Node item : graph.getNodeList()) {
-                if (item.idx == nodeToRemove.idx) {
-                    item.highlighted = false;
+                List<Node> result = new LinkedList<Node>();
+                for (Node item : tmp2) {
+                    if (!item.equals(null)) {
+                        if (item.idx != nodeToRemove.idx) {
+                            result.add(item);
+                        }
+                    }
                 }
+                result.toArray(tmp);
+                selectedNodesJList.setListData(tmp);
+
+                for (Node item : graph.getNodeList()) {
+                    if (item.idx == nodeToRemove.idx) {
+                        item.highlighted = false;
+                    }
+                }
+                graphCanvas.repaint();
             }
-            graphCanvas.repaint();
         });
 
         //adds selected nodes to textarea/jlist
@@ -73,24 +78,37 @@ public class OptionPanel extends JPanel {
 
             //funguje, ale neni to "pekne" + je to pomale
             int oldNodes = selectedNodesJList.getModel().getSize();
-            List<Node> newNodes = nodeJList.getSelectedValuesList();
-            Node[] tmp = new Node[newNodes.size() + oldNodes];
+            Node newNode = nodeJList.getSelectedValue();
+            if (newNode != null) {
+                Node[] tmp = new Node[1 + oldNodes];
 //            selectedNodesInfo.setText("");
-            int index = 0;
-            for (int i = 0; i < oldNodes; i++, index++) {
-                tmp[i] = selectedNodesJList.getModel().getElementAt(i);
-            }
-
-            for (int i = index, j = 0; j < newNodes.size(); i++, j++) {
-                if (!newNodes.get(j).highlighted){
-//                    selectedNodesInfo.append(n.toString() + "\n");
-                    tmp[i] = newNodes.get(j);
-                    newNodes.get(j).highlighted = true;
+                int index = 0;
+                for (int i = 0; i < oldNodes; i++, index++) {
+                    tmp[i] = selectedNodesJList.getModel().getElementAt(i);
                 }
-            }
 
-            selectedNodesJList.setListData(tmp);
-            graphCanvas.repaint();
+//            for (int i = index, j = 0; j < newNodes.size(); i++, j++) {
+//                if (!newNodes.get(j).highlighted) {
+////                    selectedNodesInfo.append(n.toString() + "\n");
+//                    tmp[i] = newNodes.get(j);
+//                    newNodes.get(j).highlighted = true;
+//                }
+//            }
+                if (!newNode.highlighted) {
+                    tmp[tmp.length - 1] = newNode;
+                    newNode.highlighted = true;
+                }
+
+                selectedNodesJList.setListData(tmp);
+                graphCanvas.repaint();
+            }
+        });
+
+        helpButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(this, "'ADD': Adds selected airport from 'list of all airports' to the 'list of selected airports' and highlights it.\n" +
+                    "'REMOVE': Removes selected airport from 'list of selected airports' and un-highlights it.\n" +
+                    "'HELP?': Shows this help.\n" +
+                    "'Clear All': Removes all airports from 'list of selected airports' and un-highlights them.\n", "Help", JOptionPane.QUESTION_MESSAGE);
         });
 
         //add selected node from graphCanvas
@@ -107,9 +125,9 @@ public class OptionPanel extends JPanel {
                 }
                 for (Node n :
                         graph.getNodeList()) {
-                    if (n.highlighted && !contains(tmp,n)){
+                    if (n.highlighted && !contains(tmp, n)) {
 //                        selectedNodesInfo.append(n.toString() + "\n");
-                        tmp[tmp.length-1] = n;
+                        tmp[tmp.length - 1] = n;
                         break;
                     }
                 }
@@ -128,22 +146,28 @@ public class OptionPanel extends JPanel {
         });
     }
 
-    public void createUIElements(){
+    public void createUIElements() {
         //show button - higlights selected nodes on map and shows info
 //        showButton = new JButton("Select");
 //        showButton.setName("showButton");
         addButton = new JButton("ADD");
-        addButton.setToolTipText("Add selected nodes");
+        addButton.setToolTipText("Add selected node from list above.");
         removeButton = new JButton("REMOVE");
-        removeButton.setToolTipText("Remove selected nodes");
+        removeButton.setToolTipText("Remove selected node form list below.");
         //clear button
         clearButton = new JButton("Clear All");
+        clearButton.setToolTipText("Clears all selected and highlighted nodes.");
+        //help button
+        helpButton = new JButton("HELP?");
+        helpButton.setToolTipText("Shows help.");
         //shows info about nodes which are hoovered over
         infoLabel = new JLabel();
-        infoLabel.setText("List of all airports to select from");
+        infoLabel.setText("List of all airports to select from: ");
+        infoLabel2 = new JLabel();
+        infoLabel2.setText("List of selected airports: ");
         //lists all nodes
         nodeJList = new JList<>(graph.getNodeList());
-        nodeJList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        nodeJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         nodeJList.setLayoutOrientation(JList.VERTICAL);
         nodeJList.setVisibleRowCount(15);
         listScroller = new JScrollPane(nodeJList);
@@ -174,16 +198,20 @@ public class OptionPanel extends JPanel {
         buttonJP.setLayout(new BoxLayout(buttonJP, BoxLayout.X_AXIS));
         addButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         removeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        helpButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         buttonJP.add(addButton);
         buttonJP.add(removeButton);
+        buttonJP.add(helpButton);
         tmpJP.setLayout(new BoxLayout(tmpJP, BoxLayout.Y_AXIS));
         infoLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        infoLabel2.setAlignmentX(Component.CENTER_ALIGNMENT);
         listScroller.setAlignmentX(Component.CENTER_ALIGNMENT);
         clearButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         textAreaScroller.setAlignmentX(Component.CENTER_ALIGNMENT);
         tmpJP.add(infoLabel);
         tmpJP.add(listScroller);
         tmpJP.add(buttonJP);
+        tmpJP.add(infoLabel2);
         tmpJP.add(textAreaScroller);
         tmpJP.add(clearButton);
 //        this.add(infoLabel);
@@ -191,7 +219,7 @@ public class OptionPanel extends JPanel {
 //        this.add(showButton);
 //        this.add(textAreaScroller);
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        tmpJP.setMaximumSize(new Dimension((Toolkit.getDefaultToolkit().getScreenSize().width /3), Toolkit.getDefaultToolkit().getScreenSize().height));
+        tmpJP.setMaximumSize(new Dimension((Toolkit.getDefaultToolkit().getScreenSize().width / 3), Toolkit.getDefaultToolkit().getScreenSize().height));
         this.add(graphCanvas);
         this.add(tmpJP);
 
@@ -200,11 +228,10 @@ public class OptionPanel extends JPanel {
 //        this.add(optionsPanel);
     }
 
-    boolean contains(Node[] nodes, Node node){
+    boolean contains(Node[] nodes, Node node) {
         for (int i = 0; i < nodes.length - 1; i++) {
             if (nodes[i].idx == node.idx) return true;
         }
         return false;
     }
-
 }
